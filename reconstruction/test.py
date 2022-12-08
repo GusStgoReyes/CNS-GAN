e36c79041aff90c7bbf23b528d08dc64ef0c8475
GusStgoReyes <gustxsr@mit.edu>
2022-12-08 09:41:35 -0800

    new test

--- reconstruction/test.py
+++ reconstruction/test.py
@@ -15,7 +15,7 @@ from torch.autograd import Variable
 from matplotlib.pyplot import imshow
 
 sys.path.append('./')
-import decode.dec_config as config
+# import decode.dec_config as config
 from itertools import product
 
 if __name__=="__main__":
@@ -37,71 +37,64 @@ if __name__=="__main__":
             x = self.layer1(x)
             x = self.layer0(x)
             return x
-
-    subjects = config.subjects
-    rois = config.rois
         
     print("-------- loading model --------")
     
     net = transnet().cuda()
-    net.load_state_dict(torch.load('./reconstruction/model/deconvNN_par.pkl'))
+    net.load_state_dict(torch.load('/content/CNS-GAN/reconstruction/model/deconvNN_par.pkl'))
 
     print("-------- reconstruction --------")
 
-    for sbj, roi in product(subjects, rois):
-        print('--------------------')
-        print('Subject:    %s' % sbj)
-        print('ROI:        %s' % roi)
 
-        decode_id = 'decode_' + sbj + '_' + roi
+    decode_id = 'decode'
 
-        with open(os.path.join(args.decoded_feat, decode_id+'_pred.pkl'), "rb") as f:
-            test_x = pickle.load(f)
+    with open(os.path.join(args.decoded_feat, decode_id+'_pred.pkl'), "rb") as f:
+        test_x = pickle.load(f)
 
-        with open(os.path.join(args.decoded_feat, decode_id+'_id.pkl'), "rb") as f:
-            test_id = pickle.load(f)
+    with open(os.path.join(args.decoded_feat, decode_id+'_id.pkl'), "rb") as f:
+        test_id = pickle.load(f)
 
-        testdata_x = []
+    testdata_x = []
 
-        for i in range(len(test_x)):
-            testdata_x.append(test_x[i].reshape(4096))
+    for i in range(len(test_x)):
+        testdata_x.append(test_x[i].reshape(4096))
 #           testdata_y.append(train_y[train_id[i]])
-    
-        print("-------- generate coarse image --------")
 
-        test_img_path = './tmp/test_pix2pix/'
+    print("-------- generate coarse image --------")
 
-        for i in range (len(testdata_x)):
-            img_class = test_id[i][0: 9]
-            file_path = os.path.join(test_img_path, img_class)
-            if not os.path.exists(file_path):
-                os.makedirs(file_path)
-            
-            index = [0]
-            index[0] = testdata_x[i]
-            x = torch.from_numpy(np.array(index)).float().cuda()
-            prediction = net(x).cpu().data
-            img = transforms.ToPILImage()(prediction[0]).convert('RGB').resize((128, 128))
-            img.save(os.path.join(file_path, test_id[i]))
+    test_img_path = '/content/CNS-GAN/tmp/test_pix2pix/'
+
+    for i in range (len(testdata_x)):
+        img_class = test_id[i][0: 9]
+        file_path = os.path.join(test_img_path, img_class)
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
         
+        index = [0]
+        index[0] = testdata_x[i]
+        x = torch.from_numpy(np.array(index)).float().cuda()
+        prediction = net(x).cpu().data
+        img = transforms.ToPILImage()(prediction[0]).convert('RGB').resize((128, 128))
+        img.save(os.path.join(file_path, test_id[i]+ '.JPEG'))
+    
 #           xxx = torch.from_numpy(testdata_y[i][0])
 #           img = transforms.ToPILImage()(xxx).convert('RGB').resize((128, 128))
 #           img.save(file_path + test_id[i])
 
-        print("-------- refining image --------")
+    # print("-------- refining image --------")
 
-        output_dir = args.output
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+    # output_dir = args.output
+    # if not os.path.exists(output_dir):
+    #     os.makedirs(output_dir)
 
-        for i in range (len(testdata_x)):
-            img_class = test_id[i][0: 9]
-            img_path = os.path.join(test_img_path, img_class)
-            if not os.path.exists(img_path):
-                print('image class %s not exist in dataset, skipping' % img_class)
-                continue
-            
-            command = "python ./reconstruction/pix2pix/test.py --dataroot "+ img_path + " --name " + img_class + " --model test --netG unet_128 --direction BtoA --dataset_mode single --norm batch --load_size 128 --crop_size 128 --checkpoints_dir ./reconstruction/model/checkpoints --results_dir " + output_dir
-            subprocess.call(command.replace('\n',''), shell=True)
+    # for i in range (len(testdata_x)):
+    #     img_class = test_id[i][0: 9]
+    #     img_path = os.path.join(test_img_path, img_class)
+    #     if not os.path.exists(img_path):
+    #         print('image class %s not exist in dataset, skipping' % img_class)
+    #         continue
+        
+    #     command = "python ./reconstruction/pix2pix/test.py --dataroot "+ img_path + " --name " + img_class + " --model test --netG unet_128 --direction BtoA --dataset_mode single --norm batch --load_size 128 --crop_size 128 --checkpoints_dir ./reconstruction/model/checkpoints --results_dir " + output_dir
+    #     subprocess.call(command.replace('\n',''), shell=True)
     
     print("-------- reconstruction complete --------")
